3e6be5df55a45f830e14234bfba308f4864cfcd7
wrmsr <timwilloney@gmail.com>
2016-04-02 01:21:53 -0700

    --

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 
 #include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/DataLayout.h>
 #include <llvm/Bitcode/ReaderWriter.h>
 #include <llvm/Support/CommandLine.h>
 #include <llvm/Support/MemoryBuffer.h>
 #include <llvm/IR/Module.h>
+#include <llvm/Pass.h>
+#include <llvm/PassManager.h>
 
 static llvm::cl::opt<std::string> input(llvm::cl::Positional, llvm::cl::desc("<input bitcode>"), llvm::cl::init("-"));
 static llvm::cl::opt<std::string> classname("classname", llvm::cl::desc("Binary name of the generated class"));
@@ -30,18 +33,30 @@ llvm::cl::opt<DebugLevel> debugLevel(
 int main(int argc, const char *const *argv) {
   llvm::cl::ParseCommandLineOptions(argc, argv, "LLJVM Backend\n");
 
-  llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> bufOrErr = llvm::MemoryBuffer::getFileOrSTDIN(input);
+  auto bufOrErr = llvm::MemoryBuffer::getFileOrSTDIN(input);
   if (bufOrErr.getError()) {
     std::cerr << "Unable to open bitcode file: " << bufOrErr.getError().message() << std::endl;
     return 1;
   }
-  std::unique_ptr<llvm::MemoryBuffer> buf = std::move(bufOrErr.get());
+  auto buf = std::move(bufOrErr.get());
 
-  llvm::ErrorOr<llvm::Module *> modOrErr = llvm::parseBitcodeFile(buf->getMemBufferRef(), llvm::getGlobalContext());
+  auto modOrErr = llvm::parseBitcodeFile(buf->getMemBufferRef(), llvm::getGlobalContext());
   if (!modOrErr) {
     std::cerr << "Unable to parse bitcode file: " << modOrErr.getError().message() << std::endl;
     return 1;
   }
+  auto *mod = std::move(modOrErr.get());
+
+  llvm::DataLayout dl(
+    "e-p:32:32:32"
+      "-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64"
+      "-f32:32:32-f64:64:64");
+  mod->setDataLayout(&dl);
+
+  llvm::PassManager pm;
+  pm.add(new llvm::DataLayoutPass());
+
+  pm.run(*mod);
 
   return 0;
 }
